e9cf3cbafe28999841b27898fe672b293e122620
devbhadresh104 <100311034+devbhadresh104@users.noreply.github.com>
2022-04-04 15:07:09 -0400

    Add files via upload
    lab exercise

---
+++ environment/collections.py/string-data-type.py
@@ -0,0 +1,14 @@
+myString= " this is a string."
+print(myString)
+print(myString + " is of the data type"  + str(type(myString)))
+firstString= "water"
+secondString= "fall"
+thirdString = firstString + secondString
+print(thirdString)
+name = ("What is your name? ")
+print(name)
+read = input()
+
+color = input(" what is your favorite color? " )
+animal= input(" what is your favorite animal? ")
+print("{}, you like {} {}!" .format(read,color,animal))
